c10883ed1ffe085c80f22315dbeb5d0ee5258346
fehmir <fehmir.smaili@uha.fr>
2021-09-14 13:18:17 +0200

    creation de la bdd + persistance

--- src/main/java/com/gestionnaire/notes/NotesApplication.java
+++ src/main/java/com/gestionnaire/notes/NotesApplication.java
@@ -1,13 +1,39 @@
 package com.gestionnaire.notes;
 
+import com.gestionnaire.notes.dao.EtudiantRepository;
+import com.gestionnaire.notes.entities.Etudiant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @SpringBootApplication
 public class NotesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NotesApplication.class, args);
+	public static void main(String[] args) throws ParseException {
+		ApplicationContext ctx = SpringApplication.run(NotesApplication.class, args);
+		EtudiantRepository etudiantRepository =
+				ctx.getBean(EtudiantRepository.class);
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+		etudiantRepository.save(
+				new Etudiant("Ahmed", "Med", df.parse("1988-11-10"),
+						"ahmed@gmail.com"));
+		etudiantRepository.save(
+				new Etudiant("Muhamed", "Med", df.parse("1988-11-10"),
+						"med@gmail.com"));
+		etudiantRepository.save(
+				new Etudiant("Ibrahim", "Med", df.parse("1988-11-10"),
+						"ibrahim@gmail.com"));
+
+		Page<Etudiant> etds = etudiantRepository.chercherEtudiantsNom("%d%", PageRequest.of(0, 3));
+		etds.forEach(e-> System.out.println(e.getNom()));
 	}
 
 }
